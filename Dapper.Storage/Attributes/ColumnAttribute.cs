5bf6aa95c31f814e274733a3670a9d2b078fcebd
Sasha <sashayudaev6@gmail.com>
2018-10-19 22:58:04 +0300

    added attributes

---
+++ Dapper.Storage/Attributes/ColumnAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dapper.Storage.Attributes
+{
+	public class ColumnAttribute : Attribute
+	{
+		public string Name { get; }
+
+		public ColumnAttribute(string name)
+		{
+			Name = name ??
+				throw new ArgumentNullException(name);
+		}
+	}
+}
---
+++ Dapper.Storage/Attributes/TableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dapper.Storage.Attributes
+{
+	public class TableAttribute
+	{
+		public string Name { get; }
+		public string Schema { get; set; }
+
+		public TableAttribute(string name)
+		{
+			Name = name ??
+				throw new ArgumentNullException(name);
+		}
+	}
+}
