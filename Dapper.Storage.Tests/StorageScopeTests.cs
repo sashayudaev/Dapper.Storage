4e3c716740f0ec27887b69dd0ba97279c72950fe
Sasha <sashayudaev6@gmail.com>
2018-10-21 15:49:51 +0300

    Added StorageScope tests

--- Dapper.Storage.Tests/StorageScopeTests.cs
+++ Dapper.Storage.Tests/StorageScopeTests.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using Dapper.Storage.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Dapper.Storage.Tests
 {
@@ -7,8 +8,35 @@ namespace Dapper.Storage.Tests
 	public class StorageScopeTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void SingleStorageScope_SameTransactionLevel_ShouldControlLevel()
 		{
+			var resource = Mock.Of<IStorageResource>();
+			var scope = new StorageScope(resource);
+
+			scope.Dispose();
+
+			Assert.AreEqual(resource.TransactionLevel, 0);
+		}
+
+		[TestMethod]
+		public void MultipleStorageScope_SameTransactionLevel_ShouldControlLevel()
+		{
+			var resource = Mock.Of<IStorageResource>();
+			var first = new StorageScope(resource);
+			var second = new StorageScope(resource);
+			var third = new StorageScope(resource);
+
+
+			first.Dispose();
+			var twoLevels = resource.TransactionLevel; 
+			second.Dispose();
+			var oneLevel = resource.TransactionLevel;
+			third.Dispose();
+			var noLevels = resource.TransactionLevel;
+
+			Assert.AreEqual(noLevels, 0);
+			Assert.AreEqual(oneLevel, 1);
+			Assert.AreEqual(twoLevels, 2);
 		}
 	}
 }
